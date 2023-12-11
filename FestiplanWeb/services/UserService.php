726fafdbff8fd2639478955e7bb6e00d50aab2ab
Tom Douaud <tom.douaud@iut-rodez.fr>
2023-12-11 17:16:55 +0100

    WIP Gestion BD

--- FestiplanWeb/services/UserService.php
+++ FestiplanWeb/services/UserService.php
@@ -1,18 +1,18 @@
+<?php
+include ('../database.php');
 class User {
-    private $pdo;
 
-    public function __construct($pdo) {
-        $this->pdo = $pdo;
+    public function __construct() {
     }
 
-    public function createUser($email, $nom, $prenom, $pwd, $login, $pdo) {
-       // inscription d'un utilisateur
+    public function createUser($email, $nom, $prenom, $pwd, $login, $pdo): bool {
+       // Inscription d'un utilisateur
        // Renvoie vrai ou faux en fonction si l'utilisateur a été inscrit.
 
         // TODO chiffrer le mot de passe
 
        try {
-           if (!utilisateurExiste($email, $pdo)) {
+           if (!$this->utilisateurExiste($email, $pwd, $pdo)) {
                $req = $pdo->prepare('INSERT INTO utilisateur (email, nom, prenom, pwd, login) VALUES (:email, :nom, :prenom, :pwd, :login)');
                $req->bindParam(':email', $email);
                $req->bindParam(':nom', $nom);
@@ -20,7 +20,6 @@ class User {
                $req->bindParam(':pwd', $pwd);
                $req->bindParam(':login', $login);
                $req->execute();
-               ));
                return true;
            } else {
                return false;
@@ -31,22 +30,22 @@ class User {
        }
    }
 
-    public function utilisateurExiste($login,$pwd) {
+    public function utilisateurExiste($login,$pwd, $pdo): bool {
        // Vérifie si l'utilisateur existe
        // Renvoie vrai ou faux en fonction si l'utilisateur a été trouvé.
-       try{
+        $utilisateurExiste = true;
+       try {
            $maRequete = $pdo->prepare("SELECT nom, prenom from clients where login = :login and pwd = :pwd");
            $maRequete->bindParam(':login', $login);
            $maRequete->bindParam(':pwd', $pwd);
            if ($maRequete->execute()) {
-               return $maRequete->rowCount() > 0;
+               $utilisateurExiste = $maRequete->rowCount() > 0;
            }
-       }
-       catch ( Exception $e ) {
+       } catch ( Exception $e ) {
            echo "<h1>Erreur de connexion à la base de données ! </h1>";
-           return false;
+           $utilisateurExiste = false;
+       } finally {
+           return $utilisateurExiste;
        }
     }
-
-
 }
