6f9e3e2858273e39e03ea65b9aab8e0b41a80b40
François SP <francois-sp@gmx.fr>
2023-12-12 17:27:43 +0100

    Dashboard responsive

--- FestiplanWeb/index.php
+++ FestiplanWeb/index.php
@@ -1,6 +1,9 @@
 <?php
-const PREFIX_TO_RELATIVE_PATH = "/all_users";
+const PREFIX_TO_RELATIVE_PATH = "/FestiplanWeb";
 require $_SERVER[ 'DOCUMENT_ROOT' ] . PREFIX_TO_RELATIVE_PATH . '/lib/vendor/autoload.php';
+
+echo $_SERVER[ 'DOCUMENT_ROOT' ].'<br>';
+echo $_SERVER[ 'DOCUMENT_ROOT' ] . PREFIX_TO_RELATIVE_PATH . '/lib/vendor/autoload.php';
 
 use application\DefaultComponentFactory;
 use yasmf\DataSource;
--- FestiplanWeb/views/dashboard.php
+++ FestiplanWeb/views/dashboard.php
@@ -1,89 +1,119 @@
 <?php
-include('../backend/functions/function_dashboard.php');
 
-function afficher_festival(int $id_festival, string $nom_festival, string $date_debut, string $date_fin, string $lien_image, array $categories)
+//STUB
+$nom_festival = 'De Scene Palais';
+$date_debut = '16/12/2023';
+$date_fin = '24/15/2023';
+$lien_image = '../static/assets/img/deScenePalais.jpg';
+$categorie = 'Musique';
+
+$festivals[] = array(
+    'id_festival' => 1,
+    'nom_festival' => $nom_festival,
+    'date_debut' => $date_debut,
+    'date_fin' => $date_fin,
+    'lien_image' => $lien_image,
+    'categorie' => $categorie
+);
+
+//STUB
+$id_spectacle = 1;
+$nom_spectacle = 'De Scene Palais';
+$duree = 120;
+
+$spectacles[] = array(
+    'id_spectacle' => $id_spectacle,
+    'nom_spectacle' => $nom_spectacle,
+    'lien_image' => $lien_image,
+    'categorie' => $categorie,
+    'duree' => $duree
+);
+
+/**
+ * Affiche un festival
+ * @param int $id_festival L'identifiant du festival
+ * @param string $nom_festival Le nom du festival
+ * @param string $date_debut La date de début du festival
+ * @param string $date_fin La date de fin du festival
+ * @param string $lien_image Le lien vers l'image du festival
+ * @param string $categorie La catégorie du festival
+ * @return void
+ */
+function afficher_festival(int $id_festival, string $nom_festival, string $date_debut, string $date_fin, string $lien_image, string $categorie): void
 {
-    echo'<div class="card-festival">';
-    echo'    <div class="haut-card">';
-    echo'        <div class="img-festival">';
-    echo'           <img src="'.$lien_image.'" alt="Image du festival '.$nom_festival.'">';
-    echo'        </div>';
-    echo'        <div class="description-festival">';
-    echo'            <p class="nom-festival">'.$nom_festival.'</p>';
-    echo'            <p>Du '.$date_debut.'</p>';
-    echo'            <p>AU '.$date_fin.'</p>';
-    echo'        </div>';
-    echo'    </div>';
-    echo'    <div class="bas-card">';
-    echo'        <div class="group-categories">';
-    echo'            <span>Cat&eacute;gories :</span>';
-    echo'            <div class="categories">';
-                        foreach ($categories as $categorie) {
-                            echo'<span>'.$categorie.'</span>';
-                        }
-    echo'            </div>';
-    echo'        </div>';
-    echo'        <div class="group-boutons">';
-    echo'            <form method="post" action=""> <!-- TODO : mettre le lien pour éditer le festival -->';
-    echo'                <input hidden name="id-festival" value="'.$id_festival.'">';
-    echo'                <div><button type="submit"><i class="fa-solid fa-pen-to-square"></i></button></div>';
-    echo'            </form>';
-    echo'            <form method="post" action=""> <!-- TODO : mettre le lien pour supprimer le festival -->';
-    echo'                <input hidden name="id-festival" value="'.$id_festival.'">';
-    echo'                <div><button type="submit"><i class="fa-solid fa-trash-can"></i></button></div>';
-    echo'            </form>';
-    echo'        </div>';
-    echo'   </div>';
-    echo'</div>';
+    echo '<div class="card-festival">';
+    echo '    <div class="haut-card">';
+    echo '        <div class="img-festival">';
+    echo '           <img src="' . $lien_image . '" alt="Image du festival ' . $nom_festival . '">';
+    echo '        </div>';
+    echo '        <div class="description-festival">';
+    echo '            <p class="nom-festival">' . $nom_festival . '</p>';
+    echo '            <p>Du ' . $date_debut . '</p>';
+    echo '            <p>Au ' . $date_fin . '</p>';
+    echo '        </div>';
+    echo '    </div>';
+    echo '    <div class="bas-card">';
+    echo '        <div class="group-categories">';
+    echo '            <span class="label-categorie">Cat&eacute;gories :</span>';
+    echo '            <span class="categorie">' . $categorie . '</span>';
+    echo '        </div>';
+    echo '        <div class="group-boutons">';
+    echo '            <form method="post" action=""> <!-- TODO : mettre le lien pour éditer le festival -->';
+    echo '                <input hidden name="id-festival" value="' . $id_festival . '">';
+    echo '                <div><button type="submit"><i class="fa-solid fa-pen-to-square"></i></button></div>';
+    echo '            </form>';
+    echo '            <form method="post" action=""> <!-- TODO : mettre le lien pour supprimer le festival -->';
+    echo '                <input hidden name="id-festival" value="' . $id_festival . '">';
+    echo '                <div><button type="submit"><i class="fa-solid fa-trash-can"></i></button></div>';
+    echo '            </form>';
+    echo '        </div>';
+    echo '   </div>';
+    echo '</div>';
 }
 
 /**
  * @param int $id_spectacle L'identifiant du spectacle
  * @param string $nom_spectacle Le nom du spectacle
  * @param string $lien_image Le lien vers l'image du spectacle
- * @param array $categories Les catégories du spectacle
+ * @param string $categorie La catégorie du spectacle
  * @param int $duree La durée du spectacle en minutes
  */
-function afficher_spectacle(int $id_spectacle, string $nom_spectacle, string $lien_image, array $categories, int $duree)
+function afficher_spectacle(int $id_spectacle, string $nom_spectacle, string $lien_image, string $categorie, int $duree): void
 {
-    echo'<div class="card-spectacles">';
-    echo'    <div class="img-spectacle">';
-    echo'        <img src="'.$lien_image.'" alt="L\'image du spectacle '.$nom_spectacle.'">';
-    echo'    </div>';
-    echo'    <div class="description-spectacle">';
-    echo'        <p class="nom-spectacle">'.$nom_spectacle.'</p>';
-    echo'        <div class="group-categories">';
-    echo'            <span>Cat&eacute;gories :</span>';
-    echo'            <div class="categories">';
-                        foreach ($categories as $categorie) {
-                            echo'<span>'.$categorie.'</span>';
-                        }
-    echo'            </div>';
-    echo'        </div>';
-    echo'        <div class="duree">';
-    echo'            <span class="label-duree">Dur&eacute;e :</span>';
-    echo'            <span>'.minutesToHHMM($duree).'</span>';
-    echo'        </div>';
-    echo'        <div class="group-boutons">';
-    echo'            <form method="post" action=""> <!-- TODO : mettre le lien pour supprimer le spectacle -->';
-    echo'                <input hidden name="id-spectacle" value="'.$id_spectacle.'">';
-    echo'                <div>';
-    echo'                    <button type="submit">';
-    echo'                        <i class="fa-solid fa-trash-can"></i>';
-    echo'                    </button>';
-    echo'                </div>';
-    echo'            </form>';
-    echo'            <form method="post" action=""> <!-- TODO : mettre le lien pour éditer le spectacle -->';
-    echo'                <input hidden name="id-spectacle" value="'.$id_spectacle.'">';
-    echo'                <div>';
-    echo'                    <button type="submit">';
-    echo'                        <i class="fa-solid fa-pen-to-square"></i>';
-    echo'                    </button>';
-    echo'                </div>';
-    echo'            </form>';
-    echo'        </div>';
-    echo'    </div>';
-    echo'</div>';
+    echo '<div class="card-spectacles">';
+    echo '    <div class="img-spectacle">';
+    echo '        <img src="' . $lien_image . '" alt="L\'image du spectacle ' . $nom_spectacle . '">';
+    echo '    </div>';
+    echo '    <div class="description-spectacle">';
+    echo '        <p class="nom-spectacle">' . $nom_spectacle . '</p>';
+    echo '        <div class="group-categories">';
+    echo '            <span class="label-categorie">Cat&eacute;gories :</span>';
+    echo '            <span class="categorie">' . $categorie . '</span>';
+    echo '        </div>';
+    echo '        <div class="duree">';
+    echo '            <span class="label-duree">Dur&eacute;e :</span>';
+    echo '            <span>' . minutesToHHMM($duree) . '</span>';
+    echo '        </div>';
+    echo '        <div class="group-boutons">';
+    echo '            <form method="post" action=""> <!-- TODO : mettre le lien pour supprimer le spectacle -->';
+    echo '                <input hidden name="id-spectacle" value="' . $id_spectacle . '">';
+    echo '                <div>';
+    echo '                    <button type="submit">';
+    echo '                        <i class="fa-solid fa-trash-can"></i>';
+    echo '                    </button>';
+    echo '                </div>';
+    echo '            </form>';
+    echo '            <form method="post" action=""> <!-- TODO : mettre le lien pour éditer le spectacle -->';
+    echo '                <input hidden name="id-spectacle" value="' . $id_spectacle . '">';
+    echo '                <div>';
+    echo '                    <button type="submit">';
+    echo '                        <i class="fa-solid fa-pen-to-square"></i>';
+    echo '                    </button>';
+    echo '                </div>';
+    echo '            </form>';
+    echo '        </div>';
+    echo '    </div>';
+    echo '</div>';
 }
 
 /**
@@ -107,11 +137,11 @@ $id_gestionnaire_festival = 1;
         <meta name="viewport">
         <title>Festiplan - Dashboard</title>
 
-        <link rel="stylesheet" href="../../src/style/css/dashboard.css">
+        <link rel="stylesheet" href="../static/style/css/dashboard.css">
 
         <!-- Fontawesome -->
         <!-- TODO CDN -->
-        <link rel="stylesheet" href="../framework/fontawesome-free-6.2.1-web/css/all.css">
+        <link rel="stylesheet" href="../../framework/fontawesome-free-6.2.1-web/css/all.css">
 
         <script src="../static/scripts/index.js" defer></script>
     </head>
@@ -131,8 +161,8 @@ $id_gestionnaire_festival = 1;
             </div>
         </div>
     </header>
-
     <div class="wrapper">
+        <!-- Liste des festivals -->
         <div class="container entete-section">
             <h1>Mes festivals</h1>
             <a href=""> <!-- TODO : Lien vers la page de création de festival -->
@@ -143,25 +173,24 @@ $id_gestionnaire_festival = 1;
             </a>
         </div>
 
-        <!-- Liste des festivals -->
         <div class="container container-card-festivals">
             <?php
-
-            $festivals = get_festivals($id_gestionnaire_festival);
-
             foreach ($festivals as $festival) {
                 $id_festival = $festival['id_festival'];
                 $nom_festival = $festival['nom_festival'];
                 $date_debut = $festival['date_debut'];
                 $date_fin = $festival['date_fin'];
                 $lien_image = $festival['lien_image'];
-                $categories = $festival['categories'];
-                afficher_festival($id_festival, $nom_festival, $date_debut, $date_fin, $lien_image, $categories);
+                $categorie = $festival['categorie'];
+                afficher_festival($id_festival, $nom_festival, $date_debut, $date_fin, $lien_image, $categorie);
+                afficher_festival($id_festival, $nom_festival, $date_debut, $date_fin, $lien_image, $categorie);
+                afficher_festival($id_festival, $nom_festival, $date_debut, $date_fin, $lien_image, $categorie);
             }
             ?>
 
         </div>
 
+        <!-- Liste des spectacles -->
         <div class="container entete-section">
             <h1>Mes Spectacles</h1>
             <a href=""> <!-- TODO : Lien vers la page de création de spectacle -->
@@ -171,25 +200,22 @@ $id_gestionnaire_festival = 1;
                 </div>
             </a>
         </div>
-
-        <!-- Liste des spectacles -->
         <div class="container container-card-spectacles">
             <?php
-            $spectacles = get_spectacles($id_gestionnaire_festival);
 
             foreach ($spectacles as $spectacle) {
                 $id_spectacle = $spectacle['id_spectacle'];
                 $nom_spectacle = $spectacle['nom_spectacle'];
                 $lien_image = $spectacle['lien_image'];
-                $categories = $spectacle['categories'];
+                $categorie = $spectacle['categorie'];
                 $duree = $spectacle['duree'];
-                afficher_spectacle($id_spectacle, $nom_spectacle, $lien_image, $categories, $duree);
+                afficher_spectacle($id_spectacle, $nom_spectacle, $lien_image, $categorie, $duree);
+                afficher_spectacle($id_spectacle, $nom_spectacle, $lien_image, $categorie, $duree);
+                afficher_spectacle($id_spectacle, $nom_spectacle, $lien_image, $categorie, $duree);
             }
             ?>
         </div>
     </div>
-
-
     <footer>
         <!-- TODO : Vague -->
     </footer>
