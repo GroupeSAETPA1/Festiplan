57b86da6e4ef7cd01e5adbba0947552fa58ffa6a
quentinformatique <quentin161204@gmail.com>
2023-12-12 10:05:43 +0100

    ajout de components

---
+++ FestiplanWeb/static/components/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="text-footer">
+        Copyrigth © 2023 - Festiplan - Tous droits réservés
+    </div>
+</footer>
--- FestiplanWeb/views/index.php
+++ FestiplanWeb/views/index.php
@@ -106,10 +106,6 @@
         </div>
     </div>
     <div class="spacer ratio-100 wave-white-to-grey"></div>
-    <footer>
-        <div class="text-footer">
-            Copyrigth © 2023 - Festiplan - Tous droits réservés
-        </div>
-    </footer>
+    <?php include_once "../static/components/footer.php" ?>
 </body>
 </html>
