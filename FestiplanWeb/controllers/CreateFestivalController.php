8cac529352d20676c5b65483f3a2030e37676323
Lucas <lucas.descriaud@iut-rodez.fr>
2023-12-13 15:46:00 +0100

    methode description et date OK

--- FestiplanWeb/controllers/CreateFestivalController.php
+++ FestiplanWeb/controllers/CreateFestivalController.php
@@ -5,10 +5,14 @@ use PDO;
 use services\CreateFestivalService;
 use yasmf\HttpHelper;
 use yasmf\View;
+use DateTime;
 
 /**
  * Default controller
  */
+const  longueur_nom_festival = 150 ;
+
+const longueur_max_description = 1000 ;
 class CreateFestivalController {
 
     private CreateFestivalService $createFestivalService;
@@ -20,5 +24,29 @@ class CreateFestivalController {
     public function index(PDO $pdo): View{
         $view = new View("views/createFestival.php");
         return $view;
+    }
+
+    public function tousChampsOK()
+    {
+        $tousOk = $this->nomOk($_POST['nom'])
+                  && $this->descriptionOk($_POST['description'])
+                  && $this->dateOk($_POST['ddd'], $_POST['ddf']);
+    }
+
+    public function nomOk($aVerifier)
+    {
+        return $aVerifier != '' and strlen($aVerifier) <= longueur_nom_festival;
+    }
+
+    public function descriptionOk($description)
+    {
+        return $description  != '' and strlen($description) <= longueur_max_description ;
+    }
+
+    public function dateOk(mixed $ddd, mixed $ddf)
+    {
+        $debut = DateTime::createFromFormat('d/m/Y' , $ddd);
+        $fin = DateTime::createFromFormat('/d/m/Y' , $ddf);
+        return $debut < $fin ;
     }
 }
