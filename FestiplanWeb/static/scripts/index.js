a329d7de418cb974cfc210c74844fc66d72035d4
quentinformatique <quentin161204@gmail.com>
2023-12-13 17:48:19 +0100

    fix component

--- FestiplanWeb/static/scripts/index.js
+++ FestiplanWeb/static/scripts/index.js
@@ -1,5 +1,4 @@
 $('#switchToSignup').click(function() {
-    console.log("switchToSignup")
     const TL = gsap.timeline({paused: false});
 
     // animation : la presentation se décale vers la gauche
--- FestiplanWeb/views/index.php
+++ FestiplanWeb/views/index.php
@@ -115,16 +115,7 @@
         </div>
 
     </div>
-</div>
-
-<!--<?php include_once "/Festiplan/FestiplanWeb/static/components/footer.php" ?>-->
-<div class="footer">
-    <div class="spacer ratio wave-white-to-grey"></div>
-    <footer>
-        <div class="text-footer">
-            Copyrigths © 2023 - Festiplan - Tous droits réservés
-        </div>
-    </footer>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/Festiplan/FestiplanWeb/static/components/footer/footer-absolute.php" ?>
 </div>
 
 </body>
