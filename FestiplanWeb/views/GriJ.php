07b721cdb36024b875d773fae2f9acb018e1ced5
Tom Douaud <tom.douaud@iut-rodez.fr>
2023-12-19 08:22:50 +0100

    Début de la GriJ

---
+++ FestiplanWeb/views/GriJ.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport">
+        <title>Festiplan - GriJ</title>
+
+        <link rel="stylesheet" href="/Festiplan/FestiplanWeb/static/style/css/svg.css">
+        <link rel="stylesheet" href="/Festiplan/FestiplanWeb/static/style/css/components/footer.css">
+        <link rel="stylesheet" href="/Festiplan/FestiplanWeb/static/style/css/components/header.css">
+
+        <!-- Fontawesome --><!-- TODO Custom Kit -->
+
+        <link rel="stylesheet" href="/Festiplan/FestiplanWeb/framework/fontawesome-free-6.2.1-web/css/all.css">
+        <!-- Font Awesome -->
+        <link rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+              integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+              crossorigin="anonymous"
+              referrerpolicy="no-referrer"/>
+
+        <!-- Scripts -->
+        <!-- GSAP -->  <!-- Jquery -->
+        <script src="/Festiplan/node_modules/gsap/dist/gsap.min.js"></script>
+        <script src="/Festiplan/node_modules/jquery/dist/jquery.min.js"></script>
+
+        <!-- Fullcalendar (pas sur que ca soit le bon lien) TODO -->
+        <script src="/Festiplan/node_modules/fullcalendar/dist/index.global.min.js"></script>
+
+        <script src="/Festiplan/FestiplanWeb/static/scripts/redirection_logo.js" defer></script>
+        <script src="/Festiplan/FestiplanWeb/static/scripts/responsive/footerResponsive.js" defer></script>
+    </head>
+
+    <body>
+    <div class="app">
+
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/Festiplan/FestiplanWeb/static/components/header.php" ?>
+        <div class="wrapper">
+            <a href=""><i class="fa-solid fa-arrow-left"></i> Retour</a>
+            <h1>GriJ du festival <?php echo $nomFestival ?></h1>
+            <button>Générer Automatiquement la GriJ</button>
+            <div class="GriJ">
+                <div id="calendar">
+
+                </div>
+            </div>
+        </div>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/Festiplan/FestiplanWeb/static/components/footer.php" ?>
+    </body>
+</html>
