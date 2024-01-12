e09e74598420a7c74e134a35e6c842d5cb0dd4c5
Lucas <lucas.descriaud@iut-rodez.fr>
2024-01-12 09:14:24 +0100

    fix  style +  verif champ

--- FestiplanWeb/controllers/EditFestivalController.php
+++ FestiplanWeb/controllers/EditFestivalController.php
@@ -9,6 +9,10 @@ use services\UserService;
 use yasmf\HttpHelper;
 use yasmf\View;
 use DateTime;
+
+const  longueur_nom_festival = 150 ;
+
+const longueur_max_description = 1000 ;
 class EditFestivalController
 {
     private EditFestivalService $EditFestivalService;
@@ -16,6 +20,7 @@ class EditFestivalController
     public function __construct(EditFestivalService $editFestivalService)
     {
         $this->editFestivalService = $editFestivalService;
+        $this->categorieBD = $this->editFestivalService ->recupererCategorie();
     }
 
     public function index(PDO $pdo) {
@@ -54,4 +59,97 @@ class EditFestivalController
         $_SESSION['heure_fin_spectacle_editer'] = $festival['heure_fin_spectacles'];
         $_SESSION['duree_entre_spectacle'] = $festival['duree_entre_spectacle'];
     }
+
+    public function editerFestival() {
+        var_dump($_POST);
+        $nomOk = $this -> nomOk(HttpHelper::getParam('nom'));
+        $decriptionOk = $this -> descriptionOk(HttpHelper::getParam('description'));
+        $categorieOk = $this -> categorieOk(HttpHelper::getParam('categorie'));
+        $dateOk = $this -> dateOK(HttpHelper::getParam('ddd') , HttpHelper::getParam('ddf'));
+        $grijOk = $this -> grijOk(HttpHelper::getParam('heure_debut_spectacle') ,
+            HttpHelper::getParam('heure_fin_spectacle') ,
+            HttpHelper::getParam("duree_entre_spectacle"));
+        $illustrationOk = $this -> photoOk(HttpHelper::getParam('illustration'));
+        //echo 'grij : ';
+        var_dump($illustrationOk);
+        die();
+    }
+
+    public function nomOk($nom) {
+        return $nom != "" and strlen($nom) <= longueur_nom_festival;
+    }
+
+    public function descriptionOk($description) {
+        return $description != "" and strlen($description) <= longueur_max_description;
+    }
+
+    public function categorieOk($categorie) {
+        foreach ($this -> categorieBD as $categorieValide) {
+            if ($categorieValide['id_categorie'] == $categorie) {
+                return true ;
+            }
+        }
+        return false;
+    }
+
+    public function dateOk(mixed $ddd, mixed $ddf)
+    {
+        var_dump($ddd);
+        $debut = DateTime::createFromFormat('Y-m-d' , $ddd);
+        $fin = DateTime::createFromFormat('Y-m-d' , $ddf);
+        return $debut <= $fin ;
+    }
+
+    public function grijOk($debut , $fin , $pause) {
+
+        $heure_min = strtotime("00:00:00");
+        $heure_max = strtotime("23:59:59");
+        return preg_match('/^(?:2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$/', $debut)
+            && preg_match('/^(?:2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$/', $fin)
+            && strtotime($debut) >= $heure_min && strtotime($debut) <= $heure_max
+            && strtotime($fin) >= $heure_min && strtotime($fin) <= $heure_max
+            && strtotime($debut) < strtotime($fin) && $pause >= 0 ;
+
+    }
+
+    public function photoOk($nomFestival) {
+        //photo ajoute
+        if (isset($_FILES['imageFestival']) && $_FILES['imageFestival']['name'] != '') {
+            $dossier = $_SERVER[ 'DOCUMENT_ROOT' ] . PREFIX_TO_RELATIVE_PATH . '/datas/img';
+            try {
+                $extension = $this->recupererExtension($_FILES['imageFestival']['name']);
+            } catch (Exception) {
+                return false ;
+            }
+            $nouveau_nom = $nomFestival."_image".time().$extension;
+            if (move_uploaded_file($_FILES['imageFestival']['tmp_name'] , $dossier."/".$nouveau_nom)) {
+                $_SESSION['photoFestival'] = $nouveau_nom;
+                return true ;
+            } else {
+                return false;
+            }
+            // photo non ajouté
+        } else {
+            $_SESSION['photoFestival'] = 'null' ;
+            return true ;
+        }
+    }
+
+    public function recupererExtension($nomFichier) {
+        $extensionsPossibles = array(
+            strtoupper('.jpg') ,
+            strtoupper('.jpeg') ,
+            strtoupper('.gif') ,
+            strtoupper('.png'));
+        $extensionFichier = strtoupper(strrchr($nomFichier , '.'));
+        if (in_array($extensionFichier , $extensionsPossibles)) {
+            return $extensionFichier;
+        } else {
+            throw new Exception("Extension de fichier non valide");
+        }
+    }
+
+
+
 }
+
